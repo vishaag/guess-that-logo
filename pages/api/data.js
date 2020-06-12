9958dc103eb876b64f292c58c41c88d83687024d
vishaag <vishaag@gmail.com>
2020-06-12 16:45:58 +0800

    move data file to /decks

--- pages/api/data.js
+++ pages/api/data.js
@@ -9,7 +9,7 @@ const getQuestionsData = (fileContents) => {
 }
 
 export default function handler(req, res) {
-  const dataDirectory = path.join(process.cwd(), 'pages/api/data.json')
+  const dataDirectory = path.join(process.cwd(), 'decks/data.json')
   const fileContents = JSON.parse(fs.readFileSync(dataDirectory, 'utf8'))
   const data = getQuestionsData(fileContents['data']);
   res.status(200).json(data)
--- pages/api/getAnswer.js
+++ pages/api/getAnswer.js
@@ -1,20 +1,6 @@
 import fs from 'fs'
 import path from 'path'
 
-const answerSheet = {
-  0: 1,
-  1: 2,
-  2: 0,
-  3: 1,
-  4: 1,
-  5: 3,
-  6: 3,
-  7: 2,
-  8: 0,
-  9: 3,
-  10: 2
-}
-
 const getAnswer = (fileContents, questionId) => {
   const answer = fileContents.find(object => object.id == questionId).answer;
   return { "answer": answer };
@@ -22,7 +8,7 @@ const getAnswer = (fileContents, questionId) => {
 
 export default function handler(req, res) {
   const { questionId } = req.query
-  const dataDirectory = path.join(process.cwd(), 'pages/api/data.json')
+  const dataDirectory = path.join(process.cwd(), 'decks/data.json')
   const fileContents = JSON.parse(fs.readFileSync(dataDirectory, 'utf8'))
   const answer = getAnswer(fileContents['data'], questionId);
   res.status(200).json(answer) 
